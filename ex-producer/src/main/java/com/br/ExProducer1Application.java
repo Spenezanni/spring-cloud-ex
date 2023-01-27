7741786952f659689c0f2ae172c47fe3c6494504
Spenezanni <gustavospenezanni@gmail.com>
2023-01-27 16:39:29 -0300

    Microservices are runnig ok, with configuration of properties

---
+++ ex-producer/src/main/java/com/br/ExProducer1Application.java
@@ -0,0 +1,13 @@
+package com.br;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExProducer1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExProducer1Application.class, args);
+	}
+
+}
